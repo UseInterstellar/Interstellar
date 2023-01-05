d63f96e512b5ed5531e9995f6e729d406bd686dc
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-01-05 18:07:13 -0500

    added ultraviolet

---
+++ public/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/service/',
+    bare: '/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
