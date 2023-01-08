e19c6725e7ff4753dbb59b1b2c42b92ddf4cf6ec
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-01-08 00:18:26 -0500

    v0.0.1

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,18 +1,14 @@
-function myFunction() {
-    // Declare variables
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("mySearch");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("test");
-    li = ul.getElementsByClassName("card");
-  
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("p3")[0];
-      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
+// JavaScript code
+function search_animal() {
+  let input = document.getElementById("searchbar").value;
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName("animals");
+
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    } else {
+      x[i].style.display = "block";
     }
   }
+}
