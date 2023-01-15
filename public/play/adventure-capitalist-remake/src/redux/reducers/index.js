61983c00e8693a6c6cc078ff0c6fe9c7e36e0a21
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-01-14 19:02:29 -0500

    fixed ad cap

--- public/play/adventure-capitalist-remake/src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import balance from "./balance";
-import businesses from "./businesses";
-import managers from "./managers";
-import awayEarning from "./awayEarning";
-
-export default combineReducers({ balance, businesses, managers, awayEarning });
