01f0b34e8ead1942304b7d91e989284a8c62e734
Junior <juniorbutyeah@gmail.com>
2023-01-21 16:54:47 -0600

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./public/**/*.html"],
+  corePlugins: {
+    preflight: false,
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
