72471fdf57a1551e076b64ae0bf10b31ff8e7ea9
joetheshmoe <72777401+joetheshmoe@users.noreply.github.com>
2023-01-22 12:20:20 -0800

    Update go.js

--- public/assets/js/go.js
+++ public/assets/js/go.js
@@ -1,5 +1,4 @@
 function go(value) {
-  console.log("go() called with value: ", value);
   let iframe = document.querySelector(".iframe.active");
   window.navigator.serviceWorker
     .register("./sw.js", {
@@ -10,7 +9,8 @@ function go(value) {
       if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
       else if (!(url.startsWith("https://") || url.startsWith("http://")))
         url = "https://" + url;
-      document.querySelector("#iframeid").src = __uv$config.encodeUrl(url);
+      //pass the encoded url to the second page
+      sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
       location.href = "widgetbot.html";
     });
 }
