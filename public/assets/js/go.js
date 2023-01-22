726d63fc480a6b0453f1af8102ec1a6bc1c57e0c
joetheshmoe <72777401+joetheshmoe@users.noreply.github.com>
2023-01-22 12:06:29 -0800

    Update go.js

--- public/assets/js/go.js
+++ public/assets/js/go.js
@@ -9,7 +9,9 @@ function go(value) {
       if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
       else if (!(url.startsWith("https://") || url.startsWith("http://")))
         url = "https://" + url;
-      // redirect the user to the widgetbot page and include the encoded URL as a query parameter
-      location.href = "widgetbot.html?url=" + encodeURIComponent(__uv$config.encodeUrl(url));
+      // Update the iframe src before redirecting the user
+      document.querySelector("#iframeId").src = __uv$config.encodeUrl(url);
+      // redirect the user to the widgetbot page
+      location.href = "widgetbot.html";
     });
 }
