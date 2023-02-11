1aea8136311ab295e83c6f52adea86337ec6869d
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-02-11 16:25:29 -0500

    bug fixes and roblox test

--- public/assets/js/settings/themes.js
+++ public/assets/js/settings/themes.js
@@ -1,7 +1,17 @@
 document.getElementById("Themes").onchange = function()
 {
-    if(this.value === "Midnight") {
-  localStorage.setItem('midnight', 'on');
+  if(this.value === "Classic") {
+  localStorage.setItem('Classic', 'on');
+	document.body.style.backgroundColor = "#111";
+	document.body.style.color = "#ffffff";
+	document.body.style.setProperty('--bg-c', "#111");
+	localStorage.setItem('Ocean', 'off');
+	window.localStorage.removeItem('Midnight');
+	window.localStorage.setItem('Ocean', 'off');
+	window.localStorage.removeItem('Ocean');
+}
+    if(this.value === "Ocean") {
+  localStorage.setItem('Ocean', 'on');
 	document.body.style.backgroundColor = "#162545";
 	document.body.style.color = "#ffffff";
 	document.body.style.setProperty('--bg-c', "#162545");
@@ -12,9 +22,9 @@ document.getElementById("Themes").onchange = function()
 }
   if(this.value === "Midnight") {
   localStorage.setItem('midnight', 'on');
-	document.body.style.backgroundColor = "#162545";
+	document.body.style.backgroundColor = "#000";
 	document.body.style.color = "#ffffff";
-	document.body.style.setProperty('--bg-c', "#162545");
+	document.body.style.setProperty('--bg-c', "#000");
 	localStorage.setItem('classic', 'off');
 	window.localStorage.removeItem('classic');
 	window.localStorage.setItem('lava', 'off');
