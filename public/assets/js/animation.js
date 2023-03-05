5ceff7e915266dde663aafe86643fbc53a85d504
joetheshmoe <72777401+joetheshmoe@users.noreply.github.com>
2023-03-05 18:27:19 -0500

    Update animation.js

--- public/assets/js/animation.js
+++ public/assets/js/animation.js
@@ -1,16 +1,27 @@
 window.onload = () => {
-    const anchors = document.querySelectorAll('a');
+  const anchors = document.querySelectorAll('a');
   const transition_el = document.querySelector('.transition');
 
   setTimeout(() => {
     transition_el.classList.remove('is-active');
   }, 500);
 
-  window.addEventListener('popstate', () => {
-    transition_el.classList.add('is-active');
+  for (let i = 0; i < anchors.length; i++) {
+    const anchor = anchors[i];
 
-    setTimeout(() => {
-      transition_el.classList.remove('is-active');
-    }, 500);
-  });
-};
+    anchor.addEventListener('click', e => {
+      e.preventDefault();
+      let target = e.target.href;
+
+      console.log(transition_el);
+
+      transition_el.classList.add('is-active');
+
+      console.log(transition_el);
+
+      setInterval(() => {
+        window.location.href = target;
+      }, 500);
+    })
+  }
+}
