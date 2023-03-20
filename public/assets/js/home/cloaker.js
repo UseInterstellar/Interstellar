4bd310750040536278cca8c4f33d20602d0cb597
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-03-20 19:28:17 -0400

    discord.gg/gointerstellar

--- public/assets/js/home/cloaker.js
+++ public/assets/js/home/cloaker.js
@@ -11,16 +11,19 @@ const popup = open("about:blank", "_blank")
 if (!popup || popup.closed) {
   alert("Popups are disabled!")
 } else {
-  const doc = popup.document
+	const doc = popup.document
   const iframe = doc.createElement("iframe")
   const style = iframe.style
-  var link = doc.createElement('link');
-  
-  link.type = 'image/x-icon';
-  link.rel = 'shortcut icon';
-  link.href = 'http://www.stackoverflow.com/favicon.ico';
-  doc.title = "Google"
 
+  master = window.parent.document;
+  head = master.getElementsByTagName("head")[0];
+  favicon = master.createElement("link");
+  favicon.rel = "shortcut icon";
+  favicon.type = "image/png";
+  favicon.href = "/assets/images/misc/gd.png";
+  head.appendChild(favicon);
+
+  doc.title = "My Drive - Google Drive"
   iframe.src = location.href
   style.position = "fixed"
   style.top = style.bottom = style.left = style.right = 0
