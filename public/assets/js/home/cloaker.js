9ff40c70367a14e80fce2072bc44b8b19b9b6687
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-03-20 19:10:10 -0400

    test

--- public/assets/js/home/cloaker.js
+++ public/assets/js/home/cloaker.js
@@ -1,28 +1,61 @@
-var tab = window.open('about:blank', '_blank');
-	tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>' + 'Interstellar' + '</title><link rel="icon" type="image/png" href="' + window.location.origin + "/favicon.ico" + '"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + window.location.origin + frameUrl + '" frameborder="0"></iframe></body></html>';
-	tab.document.close();
-	window.location.replace(redirectSite);
-    
-if (window.location.pathname == "/") {
-	const params = new Proxy(new URLSearchParams(window.location.search), {
-		get: (searchParams, prop) => searchParams.get(prop),
-	});
-	if (params.url) {
-		var frameUrl = params.url;
-	} else {
-		var frameUrl = "/index.html";
-	}
-	document.querySelector(".frame").src = window.location.origin + frameUrl;
-	if (inFrame() != true && usingFirefox() != true && localStorage.getItem("auto_cloak") == "true") {
-		var openBlank = confirm("Would you like to hide this from your history? (To disable this popup, turn off 'Automatic Hidden Mode' in Settings)");
-		if (openBlank == true) {
-			document.querySelector(".warning").style.display = "flex";
-			document.onmousedown = () => {
-				openBackup()
-			};
-			document.onkeydown = () => {
-				openBackup()
-			};
-		}
-	}
+
+let inFrame
+
+try {
+    inFrame = window !== top
+} catch (e) {
+    inFrame = true
 }
+
+if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+    const popup = open("about:blank", "_blank")
+    if (!popup || popup.closed) {
+        alert("Allow popups and redirects to hide this from showing up in your history.")
+    } else {
+        const doc = popup.document
+        const iframe = doc.createElement("iframe")
+        const style = iframe.style
+        const link = doc.createElement("link")
+
+        doc.title = "My Drive - Google Drive"
+        link.rel = "icon";
+        link.href = "/assets/images/misc/gd.png";
+        iframe.src = location.href
+        style.position = "fixed"
+        style.top = style.bottom = style.left = style.right = 0
+        style.border = style.outline = "none"
+        style.width = style.height = "100%"
+
+        doc.body.appendChild(iframe)
+        location.replace("https://google.com")
+    }
+}
+
+function clockTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClock").innerText = time;
+    document.getElementById("MyClock").textContent = time;
+    
+    setTimeout(clockTime, 1000);
+    
+};
+
