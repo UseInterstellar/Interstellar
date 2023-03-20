825dcbe50a4403579c46c69e27ca85dda4be0eee
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-03-20 19:35:25 -0400

    revert to old cloaker

--- public/assets/js/home/cloaker.js
+++ public/assets/js/home/cloaker.js
@@ -1,4 +1,4 @@
- let inFrame
+let inFrame
 
 try {
     inFrame = window !== top
@@ -7,30 +7,25 @@ try {
 }
 
 if (!inFrame && !navigator.userAgent.includes("Firefox")) {
-const popup = open("about:blank", "_blank")
-if (!popup || popup.closed) {
-  alert("Popups are disabled!")
-} else {
-	const doc = popup.document
-  const iframe = doc.createElement("iframe")
-  const style = iframe.style
+    const popup = open("about:blank", "_blank")
+    if (!popup || popup.closed) {
+        alert("Allow popups and redirects to hide this from showing up in your history.")
+    } else {
+        const doc = popup.document
+        const iframe = doc.createElement("iframe")
+        const style = iframe.style
+        const link = doc.createElement("link")
 
-  master = window.parent.document;
-  head = master.getElementsByTagName("head")[0];
-  favicon = master.createElement("link");
-  favicon.rel = "shortcut icon";
-  favicon.type = "image/png";
-  favicon.href = "/assets/images/misc/gd.png";
-  head.appendChild(favicon);
+        doc.title = "My Drive - Google Drive"
+        link.rel = "icon";
+        link.href = "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png";
+        iframe.src = location.href
+        style.position = "fixed"
+        style.top = style.bottom = style.left = style.right = 0
+        style.border = style.outline = "none"
+        style.width = style.height = "100%"
 
-  doc.title = "My Drive - Google Drive"
-  iframe.src = location.href
-  style.position = "fixed"
-  style.top = style.bottom = style.left = style.right = 0
-  style.border = style.outline = "none"
-  style.width = style.height = "100%"
-
-  doc.body.appendChild(iframe)
-  location.replace("https://google.com")
+        doc.body.appendChild(iframe)
+        location.replace("https://google.com")
     }
 }
