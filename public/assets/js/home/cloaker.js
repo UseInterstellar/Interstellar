6cb76942152dbf38ae48b7d6086172002f13c6be
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-03-20 19:52:31 -0400

    test captchas with about:blank

--- public/assets/js/home/cloaker.js
+++ public/assets/js/home/cloaker.js
@@ -1,31 +1,42 @@
-let inFrame
-
-try {
-    inFrame = window !== top
-} catch (e) {
-    inFrame = true
+function inFrame() {
+	try {
+		return window.self !== window.top;
+	} catch (e) {
+		return true;
+	}
 }
 
-if (!inFrame && !navigator.userAgent.includes("Firefox")) {
-    const popup = open("about:blank", "_blank")
-    if (!popup || popup.closed) {
-        alert("Allow popups and redirects to hide this from showing up in your history.")
-    } else {
-        const doc = popup.document
-        const iframe = doc.createElement("iframe")
-        const style = iframe.style
-        const link = doc.createElement("link")
+function usingFirefox() {
+	return navigator.userAgent.indexOf("Firefox") != -1;
+}
+var redirectSite = "https://www.google.com";
 
-        doc.title = "My Drive - Google Drive"
-        link.rel = "icon";
-        link.href = "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png";
-        iframe.src = location.href
-        style.position = "fixed"
-        style.top = style.bottom = style.left = style.right = 0
-        style.border = style.outline = "none"
-        style.width = style.height = "100%"
-
-        doc.body.appendChild(iframe)
-        location.replace("https://google.com")
-    }
+function openBackup() {
+	var tab = window.open('about:blank', '_blank');
+	tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>' + 'My Drive - Google Drive' + '</title><link rel="icon" type="image/png" href="' + window.location.origin + "/assets/images/misc/gd.png" + '"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + window.location.origin + frameUrl + '" frameborder="0"></iframe></body></html>';
+	tab.document.close();
+	window.location.replace(redirectSite);
+}
+if (window.location.pathname == "/") {
+	const params = new Proxy(new URLSearchParams(window.location.search), {
+		get: (searchParams, prop) => searchParams.get(prop),
+	});
+	if (params.url) {
+		var frameUrl = params.url;
+	} else {
+		var frameUrl = "/index.html";
+	}
+	document.querySelector(".frame").src = window.location.origin + frameUrl;
+	if (inFrame() != true && usingFirefox() != true && localStorage.getItem("auto_cloak") == "true") {
+		var openBlank = confirm("Would you like to hide this from your history?");
+		if (openBlank == true) {
+			document.querySelector(".warning").style.display = "flex";
+			document.onmousedown = () => {
+				openBackup()
+			};
+			document.onkeydown = () => {
+				openBackup()
+			};
+		}
+	}
 }
