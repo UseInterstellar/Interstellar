646dff268c9f4d4431152bff25b1ec6fdcdb4b2b
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-03-20 19:45:56 -0400

    fix

--- public/assets/js/home/cloaker.js
+++ public/assets/js/home/cloaker.js
@@ -1,22 +1,24 @@
 let inFrame
-    try {
-        return window.self !== window.top;
-    } catch (e) {
-        return true;
-    }
 
-if (!inFrame && !navigator.userAgent.includes("Firefox")) 
+try {
+    inFrame = window !== top
+} catch (e) {
+    inFrame = true
+}
+
+if (!inFrame && !navigator.userAgent.includes("Firefox")) {
     const popup = open("about:blank", "_blank")
     if (!popup || popup.closed) {
         alert("Allow popups and redirects to hide this from showing up in your history.")
-    } else 
+    } else {
+        var tab = window.open('about:blank', '_blank');
+        
+        tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>' + 'Interstellar' + '</title><link rel="icon" type="image/png" href="' + window.location.origin + "/favicon.ico" + '"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + window.location.origin + frameUrl + '" frameborder="0"></iframe></body></html>';
+        tab.document.close();
+        window.location.replace(redirectSite);
 
-
-var redirectSite = "https://www.google.com";
-
-{
-    var tab = window.open('about:blank', '_blank');
-    tab.document.documentElement.innerHTML = '<!DOCTYPE html><html><head><title>' + 'Interstellar' + '</title><link rel="icon" type="image/png" href="' + window.location.origin + "/favicon.ico" + '"><style>body {margin:0;overflow:hidden}</style></head><body><iframe width="100%" height="100%" src="' + window.location.origin + frameUrl + '" frameborder="0"></iframe></body></html>';
-    tab.document.close();
-    window.location.replace(redirectSite);
+        doc.body.appendChild(iframe)
+        location.replace("https://google.com")
+    }
 }
+
