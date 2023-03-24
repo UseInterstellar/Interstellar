4b4f9d2c20654114304ddaf702d51e8fab88bcf7
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-03-23 20:45:05 -0400

    fix 1

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-import express from "express";
-import http from "node:http";
-import createBareServer from "@tomphttp/bare-server-node";
-import path from "node:path";
-import * as dotenv from "dotenv";
-dotenv.config();
-
-const __dirname = process.cwd();
-const server = http.createServer();
-const app = express(server);
-const bareServer = createBareServer("/bare/");
-
-// Bare Server
-server.on("request", (req, res) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeRequest(req, res);
-  } else {
-    app(req, res);
-  }
-});
-
-server.on("upgrade", (req, socket, head) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeUpgrade(req, socket, head);
-  } else {
-    socket.end();
-  }
-});
-
-server.on("listening", () => {
-  console.log(`Interstellar running at http://localhost:${process.env.PORT}`);
-});
-
-server.listen({
-  port: process.env.PORT,
-});
+(async() => {
+  await import('./index.mjs');
+})();
