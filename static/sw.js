5933b346a478698f585d6d313e3c4f7ae80b9557
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-03-25 00:45:47 -0400

    porting

---
+++ static/sw.js
@@ -0,0 +1,9 @@
+importScripts("./uv/uv.bundle.js");
+importScripts("./uv/uv.config.js");
+importScripts("./uv/uv.sw.js");
+
+const sw = new UVServiceWorker();
+
+let userKey = new URL(location).searchParams.get('userkey');
+
+self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
