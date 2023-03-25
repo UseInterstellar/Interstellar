83c3882eb8c87119dfc4b5169578cfa5f42df792
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-03-25 00:05:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ import { readFileSync, existsSync } from "node:fs";
 import { hostname } from "node:os";
 
 import serveStatic from "serve-static";
-import connect from "connect";
 
-// The following message MAY NOT be removed
-console.log("Incognito\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nYou should have received a copy of the GNU General Public License\nalong with this program. If not, see <https://www.gnu.org/licenses/>.\n");
+// The following message MAY NOT be removed.
+console.log("Interstellar\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nYou should have received a copy of the GNU General Public License\nalong with this program. If not, see <https://www.gnu.org/licenses/>.\n");
 
-const app = connect();
 const bare = createBareServer("/bare/");
 var server, PORT = process.env.PORT;
 const ssl = existsSync("../ssl/key.pem") && existsSync("../ssl/cert.pem");
