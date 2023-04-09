4e0cef947bccef02c098c3bc4bd1fa2525ba77c3
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-04-09 01:53:19 -0400

    v4.1.3

--- static/assets/js/settings/name.js
+++ static/assets/js/settings/name.js
@@ -1,11 +1,9 @@
 		function saveName() {
 			const name = document.getElementById("name").value;
 			localStorage.setItem("name", name);
-			alert("Name saved!");
 		}
 		
 		function saveIcon() {
 			const icon = document.getElementById("icon").value;
 			localStorage.setItem("icon", icon);
-			alert("Icon saved!");
 		}
