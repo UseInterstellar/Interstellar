d8d4fe2e86dad65972cae006c61541d28e92e4f2
Junior <junior4u@reborn.com>
2023-04-16 00:07:28 -0500

    nodejs optimized to the max

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import cluster from "cluster";
+import os from "os";
 import express from "express";
 import http from "node:http";
 import createBareServer from "@tomphttp/bare-server-node";
@@ -6,85 +8,97 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const __dirname = process.cwd();
-const server = http.createServer();
-const app = express(server);
-const bareServer = createBareServer("/bare/");
 
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+// Increase max sockets to 1000
+http.globalAgent.maxSockets = 1000;
 
-app.use(express.static(path.join(__dirname, "static")));
+if (cluster.isMaster) {
+  const numCPUs = os.cpus().length;
+  console.log(`Master process running with PID ${process.pid}`);
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "index.html"));
-});
-
-app.get("/web", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "web.html"));
-});
-
-app.get("/play", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "play.html"));
-});
-
-app.get("/apps", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "apps.html"));
-});
-
-app.get("/math", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "math.html"));
-});
-
-app.get("/chat", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "chat.html"));
-});
-
-app.get("/go", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "go.html"));
-});
-
-app.get("/settings", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "settings.html"));
-});
-
-app.get("/donate", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "donate.html"));
-});
-
-app.get("/404", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "404.html"));
-});
-
-app.get("/*", (req, res) => {
-  res.redirect("/404");
-});
-
-// Bare Server
-server.on("request", (req, res) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeRequest(req, res);
-  } else {
-    app(req, res);
+  // Fork worker processes
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
-});
 
-server.on("upgrade", (req, socket, head) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeUpgrade(req, socket, head);
-  } else {
-    socket.end();
-  }
-});
+  // Handle worker exit events
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(
+      `Worker ${worker.process.pid} died with code ${code} and signal ${signal}`
+    );
+    console.log("Forking a new worker...");
+    cluster.fork();
+  });
+} else {
+  const server = http.createServer();
+  const app = express(server);
+  const bareServer = createBareServer("/bare/");
 
-server.on("listening", () => {
-  console.log(`Interstellar running at http://localhost:${process.env.PORT}`);
-});
+  // Serve static files with caching
+  const staticOptions = {
+    maxAge: "1d",
+    setHeaders: (res, path) => {
+      if (path.endsWith(".html")) {
+        res.setHeader("Cache-Control", "no-cache");
+      } else {
+        res.setHeader("Cache-Control", "public, max-age=86400");
+      }
+    },
+  };
+  app.use(express.static(path.join(__dirname, "static"), staticOptions));
 
-server.listen({
-  port: process.env.PORT,
-});
+  app.use(express.json());
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
 
+  // Define routes
+  const routes = [
+    { path: "/", file: "index.html" },
+    { path: "/web", file: "web.html" },
+    { path: "/play", file: "play.html" },
+    { path: "/apps", file: "apps.html" },
+    { path: "/chat", file: "chat.html" },
+    { path: "/go", file: "go.html" },
+    { path: "/settings", file: "settings.html" },
+    { path: "/donate", file: "donate.html" },
+    { path: "/404", file: "404.html" },
+  ];
+
+  // Define routes using the routes array
+  routes.forEach((route) => {
+    app.get(route.path, (req, res) => {
+      res.sendFile(path.join(__dirname, "static", route.file));
+    });
+  });
+
+  // Catch-all route
+  app.get("/*", (req, res) => {
+    res.redirect("/404");
+  });
+
+  // Bare Server
+  server.on("request", (req, res) => {
+    if (bareServer.shouldRoute(req)) {
+      bareServer.routeRequest(req, res);
+    } else {
+      app(req, res);
+    }
+  });
+
+  server.on("upgrade", (req, socket, head) => {
+    if (bareServer.shouldRoute(req)) {
+      bareServer.routeUpgrade(req, socket, head);
+    } else {
+      socket.end();
+    }
+  });
+
+  server.listen({
+    port: process.env.PORT,
+  });
+
+  console.log(`Worker process running with PID ${process.pid}`);
+}
