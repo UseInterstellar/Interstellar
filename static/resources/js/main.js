4a2e8935e3ce699c05797c5df22c96d28a55e461
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-04-20 20:48:32 -0400

    Fix

--- static/resources/js/main.js
+++ static/resources/js/main.js
@@ -24,7 +24,7 @@ if (!inFrame && !navigator.userAgent.includes("Firefox")) {
         link.rel = "icon";
         link.href = icon;
         
-        window.location.href = "main.html"
+        iframe.src = location.href + "main.html"
         style.position = "fixed"
         style.top = style.bottom = style.left = style.right = 0
         style.border = style.outline = "none"
