259a46e5bb55a534600faedda75d19cc689cedd1
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-04-21 17:58:32 -0400

    Fixed

--- static/resources/js/go.js
+++ static/resources/js/go.js
@@ -1,19 +1,3 @@
-function visit(value) {
-  let iframe = document.querySelector(".iframe.active");
-  window.navigator.serviceWorker
-    .register("./sw.js", {
-      scope: __uv$config.prefix,
-    })
-    .then(() => {
-      let url = value.trim();
-      if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
-      else if (!(url.startsWith("https://") || url.startsWith("http://")))
-        url = "https://" + url;
-        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-
-    });
-}
-
 function go(value) {
   let iframe = document.querySelector(".iframe.active");
   window.navigator.serviceWorker
@@ -29,13 +13,4 @@ function go(value) {
       sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
       location.href = "go.html";
     });
-}
-
-function isUrl(val = "") {
-  if (
-    /^http(s?):\/\//.test(val) ||
-    (val.includes(".") && val.substr(0, 1) !== " ")
-  )
-    return true;
-  return false;
 }
