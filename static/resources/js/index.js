cda0737a4c02cb3bacc5121f6727d7a1c7643ad9
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-04-22 20:44:26 -0400

    Fix?

--- static/resources/js/index.js
+++ static/resources/js/index.js
@@ -25,3 +25,36 @@ function isUrl(val = "") {
     return true;
   return false;
 }
+
+function go(value) {
+  let iframe = document.querySelector(".iframe.active");
+  window.navigator.serviceWorker
+    .register("./sw.js", {
+      scope: __uv$config.prefix,
+    })
+    .then(() => {
+      let url = value.trim();
+      if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
+      else if (!(url.startsWith("https://") || url.startsWith("http://")))
+        url = "https://" + url;
+      //pass the encoded url to the second page
+      sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
+      location.href = "go";
+    });
+}
+
+function visit(value) {
+  let iframe = document.querySelector(".iframe.active");
+  window.navigator.serviceWorker
+    .register("./sw.js", {
+      scope: __uv$config.prefix,
+    })
+    .then(() => {
+      let url = value.trim();
+      if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
+      else if (!(url.startsWith("https://") || url.startsWith("http://")))
+        url = "https://" + url;
+        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+
+    });
+}
