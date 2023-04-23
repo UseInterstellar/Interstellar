55b72fc168672f2345c411d2dff5efe28c183cdc
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-04-22 21:19:56 -0400

    Fix

---
+++ static/resources/js/go.js
@@ -0,0 +1,25 @@
+function go(value) {
+    let iframe = document.querySelector(".iframe.active");
+    window.navigator.serviceWorker
+      .register("./sw.js", {
+        scope: __uv$config.prefix,
+      })
+      .then(() => {
+        let url = value.trim();
+        if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
+        else if (!(url.startsWith("https://") || url.startsWith("http://")))
+          url = "https://" + url;
+        //pass the encoded url to the second page
+        sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
+        location.href = "go";
+      });
+  }
+  
+  function isUrl(val = "") {
+    if (
+      /^http(s?):\/\//.test(val) ||
+      (val.includes(".") && val.substr(0, 1) !== " ")
+    )
+      return true;
+    return false;
+  }
