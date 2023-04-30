3204d3ba2476a412b7a3310fad21d5350eb886d0
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-04-30 19:35:57 -0400

    Fixed

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/~/go/',
+    prefix: '/service/',
     bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
