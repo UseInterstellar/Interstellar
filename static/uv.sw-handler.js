bef99d452c979b2547e4f7c0cb2d54b67a634dcd
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-05-19 08:49:30 -0400

    Removed Arc

--- static/uv.sw-handler.js
+++
@@ -1,8 +0,0 @@
-importScripts('/uv/uv.bundle.js');
-importScripts('/uv/uv.config.js');
-importScripts(__uv$config.sw || '/uv/uv.sw.js');
-
-const sw = new UVServiceWorker();
-self.addEventListener('fetch', event => {
-    event.respondWith(sw.fetch(event))
-});
