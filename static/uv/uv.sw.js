ddc6e96f89303a26b0ef90288ab99282f4865bb4
bubbo <85169821+xbubbo@users.noreply.github.com>
2023-06-15 16:27:23 -0400

    fix

--- static/uv/uv.sw.js
+++ static/uv/uv.sw.js
@@ -1,5 +1,5 @@
-importScripts('../uv/uv.bundle.js');
-importScripts('../uv/uv.config.js');
+importScripts('/uv/uv.bundle.js');
+importScripts('/uv/uv.config.js');
 
 class UVServiceWorker extends EventEmitter {   
     constructor(config = __uv$config) {
