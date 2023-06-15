44375993fdbb64f52f8d91bdf0a9b156b650affd
bubbo <85169821+xbubbo@users.noreply.github.com>
2023-06-15 16:21:18 -0400

    fix

--- static/uv.sw-handler.js
+++ static/uv.sw-handler.js
@@ -1,6 +1,6 @@
-importScripts('./uv/uv.bundle.js');
-importScripts('./uv/uv.config.js');
-importScripts(__uv$config.sw || './uv/uv.sw.js');
+importScripts('../uv/uv.bundle.js');
+importScripts('../uv/uv.config.js');
+importScripts(__uv$config.sw || '../uv/uv.sw.js');
 
 const sw = new UVServiceWorker();
 self.addEventListener('fetch', event => {
