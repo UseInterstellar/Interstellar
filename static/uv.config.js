3c5807dac836a0fef77f5474ad14a033fd49d102
bubbo <85169821+xbubbo@users.noreply.github.com>
2023-06-15 16:06:06 -0400

    i think im smart??

--- static/uv.config.js
+++ static/uv.config.js
@@ -3,9 +3,9 @@ self.__uv$config = {
     bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv/uv.handler.js',
-    client: '/uv/uv.client.js',
-    bundle: '/uv/uv.bundle.js',
+    handler: '/uv.handler.js',
+    client: '/uv.client.js',
+    bundle: '/uv.bundle.js',
     config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js',
+    sw: '/uv.sw.js',
 };
