0ea5241c74d17166abe51be304ac3275f593e6f3
bubbo <85169821+xbubbo@users.noreply.github.com>
2023-06-15 17:58:43 -0400

    Revert "update uv"

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import http from "node:http";
-import { createBareServer } from "@tomphttp/bare-server-node";
+import createBareServer from "@tomphttp/bare-server-node";
 import path from "node:path";
 import * as dotenv from "dotenv";
 dotenv.config();
