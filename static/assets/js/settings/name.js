94265b7013831bc3c201f14c948ab4b5730415ae
bluehalooo <119090993+bluehalooo@users.noreply.github.com>
2023-06-20 09:06:51 -0700

    sugma

--- static/assets/js/settings/name.js
+++
@@ -1,9 +0,0 @@
-		function saveName() {
-			const name = document.getElementById("name").value;
-			localStorage.setItem("name", name);
-		}
-		
-		function saveIcon() {
-			const icon = document.getElementById("icon").value;
-			localStorage.setItem("icon", icon);
-		}
--- static/sw.js
+++
@@ -1,8 +0,0 @@
-importScripts("./uv/uv.bundle.js");
-importScripts("./uv/uv.config.js");
-importScripts("./uv/uv.sw.js");
-
-const sw = new UVServiceWorker();
-let userKey = new URL(location).searchParams.get('userkey');
-
-self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
