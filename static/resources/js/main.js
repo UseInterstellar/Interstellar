456c5cb981ef96a8dde5546e036d3b9e8375ae7d
bubbo <85169821+xbubbo@users.noreply.github.com>
2023-06-27 15:51:15 -0400

    fix

--- static/resources/js/main.js
+++ static/resources/js/main.js
@@ -32,6 +32,6 @@ if (!inFrame && !navigator.userAgent.includes("Firefox")) {
 
         doc.head.appendChild(link);
         doc.body.appendChild(iframe)
-        location.replace("https://clasroom.google.com")
+        location.replace("https://classroom.google.com")
     }
 }
