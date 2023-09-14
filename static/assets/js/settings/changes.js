42abb3105fbf59cd3f51314c66f8069cba3d7a38
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-09-13 21:30:20 -0400

    v5?

--- static/assets/js/settings/changes.js
+++
@@ -1,9 +0,0 @@
-function redirectToMainDomain() {
-  var currentUrl = window.location.href;
-  var mainDomainUrl = currentUrl.replace(/\/[^\/]*$/, '');
-  if (window != top) {
-    top.location.href = mainDomainUrl;
-  } else {
-    window.location.href = mainDomainUrl;
-  }
-}
