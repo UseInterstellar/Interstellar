e37dace213d2b01891d7fea72b837e8b3a0f820d
bubbo <85169821+imbubbo@users.noreply.github.com>
2023-09-14 16:52:58 -0400

    Merge Routes into the Static Folder.

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const routes = [
 
 routes.forEach((route) => {
   app.get(route.path, (req, res) => {
-    res.sendFile(path.join(__dirname, "routes", route.file));
+    res.sendFile(path.join(__dirname, "static", route.file));
   });
 });
 
