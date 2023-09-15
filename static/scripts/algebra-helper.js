4cc4a374eab665df2ca9bbc300edeb20537af152
xbubbo <commits@gointerstellar.app>
2023-09-15 17:03:30 -0400

    Added ChatGPT

--- static/scripts/algebra-helper.js
+++ static/scripts/algebra-helper.js
@@ -4,177 +4,225 @@ document.addEventListener('DOMContentLoaded', () => {
       name: "Amazon",
       link: "https://amazon.com/",
       image: "/images/icons/apps/amazon.png",
-      categories: ["all", "media"]
+      categories: ["all", "media"],
+      error: false
+
     },
     {
       name: "Chess.com",
       link: "https://chess.com",
       image: "/images/icons/chess.png",
-      categories: ["all", "games"]
+      categories: ["all", "games"],
+      error: false
     },
     {
       name: "Cool Math Games",
       link: "https://coolmathgames.com",
       image: "/images/icons/apps/coolmath.png",
-      categories: ["all", "games"]
+      categories: ["all", "games"],
+      error: false
     },
     {
       name: "Discord",
       link: "https://discord.com",
       image: "/images/icons/apps/discord.jpg",
-      categories: ["all", "social"]
+      categories: ["all", "social"],
+      error: false
     },
     {
       name: "ESPN",
       link: "https://www.espn.com/watch/",
       image: "/images/icons/apps/espn.webp",
-      categories: ["all", "media"]
+      categories: ["all", "media"],
+      error: false
     },
     {
       name: "Fifa Rosters",
       link: "https://fifarosters.com/",
       image: "/images/icons/fifa.png",
-      categories: ["all", "media"]
+      categories: ["all", "media"],
+      error: false
     },
     {
       name: "Firefox Web Browser",
       link: "https://replit.com/@cooleddie001/Firefox-Legacy?v=1",
       image: "/images/icons/apps/firefox.png",
-      categories: ["all", "media"]
+      categories: ["all", "media"],
+      error: false
     },
     {
       name: "Flix HQ",
       link: "https://flixhq.to",
       image: "/images/icons/apps/flixhq.png",
-      categories: ["all", "media"]
+      categories: ["all", "media"],
+      error: false
     },
     {
       name: "Geforce NOW",
       link: "https://play.geforcenow.com",
       image: "/images/icons/apps/geforce-now.png",
-      categories: ["all", "stream", "cloud"]
+      categories: ["all", "stream", "cloud"],
+      error: false
     },
     {
       name: "Github",
       link: "https://github.com",
       image: "/images/icons/apps/github.png",
-      categories: ["all", "media"]
+      categories: ["all", "media"],
+      error: false
     },
     {
       name: "Google",
       link: "https://google.com",
       image: "/images/icons/apps/google.png",
-      categories: ["all", "media"]
+      categories: ["all", "media"],
+      error: false
     },
     {
       name: "HBO MAX",
       link: "https://www.hbomax.com/",
       image: "/images/icons/apps/hbo.webp",
-      categories: ["all", "stream"]
+      categories: ["all", "stream"],
+      error: false
     },
     {
       name: "Messenger",
       link: "https://messenger.com/",
       image: "/images/icons/apps/messenger.png",
-      categories: ["all", "social"]
+      categories: ["all", "social"],
+      error: false
     },
     {
       name: "Now.GG - Astroid",
       link: "https://nowgg.wtf",
       image: "/images/icons/now-gg.webp",
-      categories: ["all", "game"]
+      categories: ["all", "game"],
+      error: false
     },
     {
       name: "Paramount Plus",
       link: "https://paramountplus.com",
       image: "/images/icons/apps/paramount.png",
-      categories: ["all", "stream"]
+      categories: ["all", "stream"],
+      error: false
     },
     {
       name: "Pinterest",
       link: "https://pinterest.com",
       image: "/images/icons/apps/pinterest.png",
-      categories: ["all", "media"]
+      categories: ["all", "media"],
+      error: false
     },
     {
       name: "Pixlr",
       link: "https://pixlr.com/",
       image: "/images/icons/pix.png",
-      categories: ["all", "media"]
+      categories: ["all", "media"],
+      error: false
     },
     {
       name: "Poki",
       link: "https://poki.com",
       image: "/images/icons/apps/poki.png",
-      categories: ["all","game"]
+      categories: ["all","game"],
+      error: false
     },
     {
       name: "Soundcloud",
       link: "https://soundcloud.com",
       image: "/images/icons/apps/soundcloud.jpg",
-      categories: ["all", "media"]
+      categories: ["all", "media"],
+      error: false
     },
     {
       name: "Spotify",
       link: "https://open.spotify.com",
       image: "/images/icons/apps/spotify.png",
-      categories: ["all", "media"]
+      categories: ["all", "media"],
+      error: false
     },
     {
       name: "Telegram",
       link: "https://web.telegram.org/",
       image: "/images/icons/apps/telegram.png",
-      categories: ["all", "soc"]
+      categories: ["all", "soc"],
+      error: false
     },
     {
       name: "Tiktok",
       link: "https://tiktok.com",
       image: "/images/icons/apps/tiktok.png",
-      categories: ["all", "soc", "media"]
+      categories: ["all", "soc", "media"],
+      error: false
     },
     {
       name: "Tumblr",
       link: "https://tumblr.com/",
       image: "/images/icons/apps/tumblr.png",
-      categories: ["all", "soc"]
+      categories: ["all", "soc"],
+      error: false
     },
     {
       name: "Twitch",
       link: "https://twitch.tv",
       image: "/images/icons/apps/twitch-tv.png",
-      categories: ["all", "stream", "media"]
+      categories: ["all", "stream", "media"],
+      error: false
     },
     {
       name: "Twitter",
       link: "https://twitter.com",
       image: "/images/icons/apps/twitter.png",
-      categories: ["all", "soc", "media"]
+      categories: ["all", "soc", "media"],
+      error: false
     },
     {
       name: "VS Code",
       link: "https://vscode.dev",
       image: "/images/icons/apps/vscode.png",
-      categories: ["all", "tool"]
+      categories: ["all", "tool"],
+      error: false
     },
     {
       name: "Y8 Games",
       link: "https://y8.com/",
       image: "/images/icons/apps/y8.png",
-      categories: ["all", "game"]
+      categories: ["all", "game"],
+      error: false
     },
     {
       name: "YouTube",
       link: "https://youtube.com",
       image: "/images/icons/apps/yt.png",
-      categories: ["all", "soc", "stream", "media"]
+      categories: ["all", "soc", "stream", "media"],
+      error: false
     },
     {
       name: "Whatsapp",
       link: "https://web.whatsapp.com/",
       image: "/images/icons/apps/whatsapp.png",
-      categories: ["all", "message", "soc"]
-    }
+      categories: ["all", "message", "soc"],
+      error: false
+    },
+    {
+      name: "Cool Math Games",
+      link: "https://coolmathgames.com",
+      image: "/images/icons/apps/coolmath.png",
+      categories: ["all", "games"],
+      error: false
+    },
+    {
+      name: "ChatGPT Clone",
+      link: "https://chat.shuttle.rip/",
+      image: "/images/icons/apps/chatgpt.png",
+      categories: ["all", "AI"],
+      error: false
+    },
   ];
+    
+  
+  appsList.sort((a, b) => a.name.localeCompare(b.name));
+
   const nonPinnedApps = document.querySelector('.container-apps');
   const pinnedApps = document.querySelector('.pinned-apps');
   var pinList = localStorage.getItem("pinnedApps");
