04ccec7d6c1d105ce3d5cbdba4336d181f9fdeca
airtag8892 <120539605+airtag8892@users.noreply.github.com>
2023-09-24 15:36:33 -0400

    add neal.fun

--- static/scripts/calculator.js
+++ static/scripts/calculator.js
@@ -71,6 +71,13 @@ document.addEventListener('DOMContentLoaded', () => {
       error: false
     },
     {
+      name: "Neal.Fun",
+      link: "https://neal.fun",
+      image: "/images/icons/nealfun.png",
+      categories: ['all'],
+      error: false
+    },
+    {
       name: "FeedVid",
       link: "https://v6p9d9t4.ssl.hwcdn.net/html/4941980-683813/index.html",
       image: "/images/icons/feedvid.png",
