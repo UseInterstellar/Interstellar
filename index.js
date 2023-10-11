7992c57a7b4cc606b2a281e1e14c3b4765f15a66
bubbo <85169821+xbubbo@users.noreply.github.com>
2023-10-11 16:31:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import http from "node:http";
-import createBareServer from "@tomphttp/bare-server-node";
+import createBareServer from "bare-server-node";
 import path from "node:path";
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -65,4 +65,3 @@ server.on("listening", () => {
 server.listen({
   port: PORT,
 });
-
