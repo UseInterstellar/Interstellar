a167fc633854695bc30a87353641c016c0308d70
xbubbo <commits@gointerstellar.app>
2023-10-13 18:51:01 -0400

    🚀 | v5.0.1 (Updated Ultraviolet)

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import express from "express";
-import http from "node:http";
-import createBareServer from "@tomphttp/bare-server-node";
-import path from "node:path";
-import * as dotenv from "dotenv";
+import express from 'express';
+import http from 'node:http';
+import { createBareServer } from '@tomphttp/bare-server-node';
+import path from 'node:path';
+import * as dotenv from 'dotenv';
 dotenv.config();
 
 const __dirname = process.cwd();
 const server = http.createServer();
 const app = express(server);
-const bareServer = createBareServer("/outerspace/");
-const PORT = process.env.PORT
+const bareServer = createBareServer('/outerspace/');
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(
@@ -18,31 +18,26 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "static")));
+app.use(express.static(path.join(__dirname, 'static')));
 
 const routes = [
-  { path: "/", file: "index.html" },
-  { path: "/news", file: "apps.html" },
-  { path: "/algebra", file: "games.html" },
-  { path: "/settings", file: "settings.html" },
-  { path: "/tabs", file: "tabs.html" },
-  { path: "/tabinner", file: "tabinner.html" },
-  { path: "/go", file: "go.html" },
-  { path: "/loading", file: "loading.html" },
-  { path: "/404", file: "404.html" },
+  { path: '/', file: 'index.html' },
+  { path: '/news', file: 'apps.html' },
+  { path: '/algebra', file: 'games.html' },
+  { path: '/settings', file: 'settings.html' },
+  { path: '/tabs', file: 'tabs.html' },
+  { path: '/tabinner', file: 'tabinner.html' },
+  { path: '/go', file: 'go.html' },
+  { path: '/loading', file: 'loading.html' },
 ];
 
 routes.forEach((route) => {
   app.get(route.path, (req, res) => {
-    res.sendFile(path.join(__dirname, "static", route.file));
+    res.sendFile(path.join(__dirname, 'static', route.file));
   });
 });
 
-app.get("/*", (req, res) => {
-  res.redirect("/404");
-});
-
-server.on("request", (req, res) => {
+server.on('request', (req, res) => {
   if (bareServer.shouldRoute(req)) {
     bareServer.routeRequest(req, res);
   } else {
@@ -50,7 +45,7 @@ server.on("request", (req, res) => {
   }
 });
 
-server.on("upgrade", (req, socket, head) => {
+server.on('upgrade', (req, socket, head) => {
   if (bareServer.shouldRoute(req)) {
     bareServer.routeUpgrade(req, socket, head);
   } else {
@@ -58,11 +53,10 @@ server.on("upgrade", (req, socket, head) => {
   }
 });
 
-server.on("listening", () => {
+server.on('listening', () => {
   console.log(`Running at http://localhost:${process.env.PORT}`);
 });
 
 server.listen({
   port: PORT,
 });
-
