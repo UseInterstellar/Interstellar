2af0fe9f5ad060b68e4e6113242bdca411a6deff
xbubbo <mrnicebub432@gmail.com>
2023-10-13 18:51:01 -0400

    🚀 | v5.0.1 (Updated Ultraviolet)

--- static/sw.js
+++ static/sw.js
@@ -1,8 +1,14 @@
-importScripts("/h/mathematics.js");
-importScripts("/h/geography.js");
-importScripts("/h/english.js");
+/*global UVServiceWorker,__uv$config*/
+/*
+ * Stock service worker script.
+ * Users can provide their own sw.js if they need to extend the functionality of the service worker.
+ * Ideally, this will be registered under the scope in uv.config.js so it will not need to be modified.
+ * However, if a user changes the location of uv.bundle.js/uv.config.js or sw.js is not relative to them, they will need to modify this script locally.
+ */
+importScripts('/about-us/uv.bundle.js');
+importScripts('/about-us/uv.config.js');
+importScripts(__uv$config.sw || '/about-us/uv.sw.js');
 
 const sw = new UVServiceWorker();
-let userKey = new URL(location).searchParams.get('userkey');
 
-self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
+self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
