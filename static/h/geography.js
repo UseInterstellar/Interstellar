3949538d8032545ec34b37f3a36f8d538bdb0719
bubbo <85169821+xbubbo@users.noreply.github.com>
2023-10-14 00:01:56 -0400

    🚀 | v5.0.1 (Reverted UV Update)

---
+++ static/h/geography.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/astronomy/',
+    bare: '/outerspace/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/h/algebra.js',
+    bundle: '/h/mathematics.js',
+    config: '/h/geography.js',
+    sw: '/h/english.js',
+};
