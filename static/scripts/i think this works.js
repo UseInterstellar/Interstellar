7ef7d8f6cf041043dd2e4441c2390bbc0af5e746
Arc202 <130196372+Arc202@users.noreply.github.com>
2023-11-11 11:32:06 -0800

    Update i think this works.js

--- static/scripts/i think this works.js
+++ static/scripts/i think this works.js
@@ -2,12 +2,11 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
     
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    print("endpoint connected")
 
 @socketio.event
 def my_event(message):
