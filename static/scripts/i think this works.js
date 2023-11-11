cc6465cf57085c1be9c6a8f943db3d1414c7226e
Arc202 <130196372+Arc202@users.noreply.github.com>
2023-11-11 11:30:27 -0800

    workings.js

--- static/scripts/i think this works.js
+++ static/scripts/i think this works.js
@@ -1,1 +1,17 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+    
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.event
+def my_event(message):
+    emit('my response', {'data': 'got it!'})
+
+if __name__ == '__main__':
+    socketio.run(app)
