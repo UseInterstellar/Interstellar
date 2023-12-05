923e04a63b932a49d1e75ef42bce2ce9c817564c
xbubbo <mrnicebub432@gmail.com>
2023-12-05 06:20:22 -0500

    Change Bare Route

--- static/contact/geography.js
+++ static/contact/geography.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/reviews/',
-    bare: '/outerspace/',
+    bare: '/space/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/contact/algebra.js',
