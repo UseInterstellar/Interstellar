9b6d5ce2bb808779c496a246807cfcc4c746b8d5
tgt <mashoor539@gmail.com>
2023-12-11 16:48:06 +0000

    ughhh

--- static/contact/config.js
+++ static/contact/config.js
@@ -8,3 +8,10 @@ self.__uv$config = {
     config: '/contact/config.js',
     sw: '/contact/sw.js',
 };
+function injectEruda (code) {
+    const script = document.createElement('script');
+    script.textContent = code;
+    document.body.appendChild(script);
+    iframeId.contentDocument.inject(code)
+    
+}
