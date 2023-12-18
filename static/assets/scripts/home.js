f612e5254c14b9c1584a708fb13819facacf4e91
thonk <101288516+VillainsRule@users.noreply.github.com>
2023-12-18 17:34:57 -0500

    illegal return statement my ***

--- static/assets/scripts/home.js
+++ static/assets/scripts/home.js
@@ -8,15 +8,16 @@ try {
 
 if (!inFrame && !navigator.userAgent.includes('Firefox')) {
     const popup = open('about:blank', '_blank')
-    if (!popup || popup.closed) return alert('Please allow popups and redirects.');
-
-    popup.document.write(`
-        <iframe src="${location.href}" style="position:fixed;top:0;left:0;width:100%;height:100%;outline:none;border:none;"></iframe>
-        <title>${localStorage.getItem('name') || 'My Drive - Google Drive'}</title>
-        <link rel="icon" href="${localStorage.getItem('icon') || 'https://ssl.gstatic.com/assets/media/branding/product/1x/drive_2020q4_32dp.png'}">
-    `);
+    if (!popup || popup.closed) alert('Please allow popups and redirects.');
+    else {
+        popup.document.write(`
+            <iframe src="${location.href}" style="position:fixed;top:0;left:0;width:100%;height:100%;outline:none;border:none;"></iframe>
+            <title>${localStorage.getItem('name') || 'My Drive - Google Drive'}</title>
+            <link rel="icon" href="${localStorage.getItem('icon') || 'https://ssl.gstatic.com/assets/media/branding/product/1x/drive_2020q4_32dp.png'}">
+        `);
     
-    location.replace(localStorage.getItem('panicLink') || 'https://classroom.google.com');
+        location.replace(localStorage.getItem('panicLink') || 'https://classroom.google.com');
+    };
 };
 
 document.addEventListener("DOMContentLoaded", function(event) { 
