14a035263129cb4a9b829f23c44c585af50da4c4
incognitotgt <mashoor539@icloud.com>
2024-01-19 17:20:09 -0500

    feat: update uv to 1.0.11

--- static/sw.js
+++ static/sw.js
@@ -1,8 +1,8 @@
-importScripts('/m/bundle.js')
-importScripts('/m/config.js')
-importScripts('/m/sw.js')
+/*global UVServiceWorker,__uv$config*/
+importScripts('/m/bundle.js');
+importScripts('/m/config.js');
+importScripts(__uv$config.sw || '/m/sw.js');
 
-const sw = new UVServiceWorker()
-let userKey = new URL(location).searchParams.get('userkey')
+const sw = new UVServiceWorker();
 
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)))
+self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
