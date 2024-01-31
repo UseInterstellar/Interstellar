bf03ea100e0befbf6dcdc949065b32c9088091ab
keylen999 <148836768+keylen999@users.noreply.github.com>
2024-01-30 22:53:04 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
-  challenge: false,
+  challenge: true,
   users: {
     // username: 'password', you can add multiple users.
-    interstellar: 'password',
+    kam: '9764',
   },
 }
 export default config
