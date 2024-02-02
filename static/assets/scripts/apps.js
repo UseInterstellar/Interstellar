45fe17710130f1a1ea764e7adc3d303642556263
bruhwhyamisobad <124473366+bruhwhyamisobad@users.noreply.github.com>
2024-02-02 13:19:18 -0800

    Fix Movie Web icon

--- static/assets/scripts/apps.js
+++ static/assets/scripts/apps.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
     {
       name: 'Movie Web',
       link: 'https://movie-web.app',
-      image: 'https://movie-web.app/apple-touch-icon.webp',
+      image: 'https://movie-web.app/apple-touch-icon.png',
       categories: ['all', 'media', 'stream'],
     },
     {
