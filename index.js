efaa5fdd67035e37f2071486888798ed5692ea71
BoiJesusKing <105741882+BoiJesusKing@users.noreply.github.com>
2024-02-09 18:03:07 +0000

    Initial commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const __dirname = process.cwd()
 const server = http.createServer()
 const app = express(server)
 const bareServer = createBareServer('/v/')
-const PORT = 8080
+const PORT = 7530
 if (config.challenge) {
   console.log('Password protection is enabled. Usernames are: ' + Object.keys(config.users))
   console.log('Passwords are: ' + Object.values(config.users))
