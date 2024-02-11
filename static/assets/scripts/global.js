918e557d9fbfc81d19b5ca3bb8350a49d2d0c900
xbubbo <commits@gointerstellar.app>
2024-02-11 17:52:05 -0500

    Cache

--- static/assets/scripts/global.js
+++ static/assets/scripts/global.js
@@ -71,16 +71,16 @@ document.addEventListener('DOMContentLoaded', function (event) {
   themeEle = document.createElement('link')
   themeEle.rel = 'stylesheet'
   if (themeid == 'catppuccinMocha') {
-    themeEle.href = '/assets/styles/themes/catppuccin/mocha.css'
+    themeEle.href = '/assets/styles/themes/catppuccin/mocha.css?v=1'
   }
   if (themeid == 'catppuccinMacchiato') {
-    themeEle.href = '/assets/styles/themes/catppuccin/macchiato.css'
+    themeEle.href = '/assets/styles/themes/catppuccin/macchiato.css?v=1'
   }
   if (themeid == 'catppuccinFrappe') {
-    themeEle.href = '/assets/styles/themes/catppuccin/frappe.css'
+    themeEle.href = '/assets/styles/themes/catppuccin/frappe.css?v=1'
   }
   if (themeid == 'catppuccinLatte') {
-    themeEle.href = '/assets/styles/themes/catppuccin/latte.css'
+    themeEle.href = '/assets/styles/themes/catppuccin/latte.css?v=1'
   }
   document.body.appendChild(themeEle)
 })
