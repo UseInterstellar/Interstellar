821b175e65f6e9296eec1288baba150255413299
xbubbo <commits@gointerstellar.app>
2024-02-14 16:13:29 -0500

    Added more config options, Formatted with Prettier, Added Gemini

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 const config = {
+  routes: true, // Change this to false if you just want to host a bare server.
+  local: true, // Change this to false to disable local assets.
   challenge: false,
   users: {
     // username: 'password', you can add multiple users.
