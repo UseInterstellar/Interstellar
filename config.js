cd7ff22246f440b1edfcc390ca8e17788f945619
RajmondKurti112 <159893027+RajmondKurti112@users.noreply.github.com>
2024-02-16 11:04:03 +0000

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,13 @@ const config = {
   challenge: true,
   users: {
     // username: 'password', you can add multiple users.
-    process.env.USER1: process.env.USER1P,
-  process.env.USER2: process.env.USER2P,
-  process.env.USER3: process.env.USER3P
-    interstellar: 'password'
   },
-}
-export default config
+};
+
+// Assigning properties dynamically
+config.users[process.env.USER1] = process.env.USER1P;
+config.users[process.env.USER2] = process.env.USER2P;
+config.users[process.env.USER3] = process.env.USER3P;
+config.users.interstellar = 'password';
+
+export default config;
