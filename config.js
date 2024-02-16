823cc4d77f10da262c44a9c9f58a9fcc83301cef
xbubbo <mrnicebub432@gmail.com>
2024-02-16 12:20:47 -0500

    Fixed config options, made password protection work with other options.

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 const config = {
-  routes: true, // Change this to false if you just want to host a bare server.
-  local: true, // Change this to false to disable local assets.
-  challenge: false,
-  users: {
-    // username: 'password', you can add multiple users.
+  challenge: false, // Set to true if you want to enable password protection.
+  users: { // You can add multiple users by doing username: 'password'. 
     interstellar: 'password',
   },
+  routes: true, // Change this to false if you just want to host a bare server.
+  local: true, // Change this to false to disable local assets.
 }
 export default config
