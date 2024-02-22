4b76756fea5826a904b5ec6ec570626726e4adc9
xbubbo <mrnicebub432@gmail.com>
2024-02-22 09:41:18 -0500

    Added Window Popout Button (#468) + Formatted

--- static/assets/scripts/frame.js
+++ static/assets/scripts/frame.js
@@ -32,6 +32,33 @@ function reload() {
   }
 }
 
+function popout() {
+  const newWindow = window.open('about:blank', '_blank')
+
+  if (newWindow) {
+    const name = localStorage.getItem('name') || 'My Drive - Google Drive'
+    const icon = localStorage.getItem('icon') || 'https://ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png'
+
+    newWindow.document.title = name
+
+    const link = newWindow.document.createElement('link')
+    link.rel = 'icon'
+    link.href = icon
+    newWindow.document.head.appendChild(link)
+
+    const newIframe = newWindow.document.createElement('iframe')
+    const style = newIframe.style
+    style.position = 'fixed'
+    style.top = style.bottom = style.left = style.right = 0
+    style.border = style.outline = 'none'
+    style.width = style.height = '100%'
+
+    newIframe.src = iframe.src
+
+    newWindow.document.body.appendChild(newIframe)
+  }
+}
+
 function erudaToggle() {
   if (!iframe) return
 
