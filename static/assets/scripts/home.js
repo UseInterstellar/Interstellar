c11641f3883a436f6d5bb1189bef05ce1883cccf
sigmafortnite <161689139+sigmafortnite@users.noreply.github.com>
2024-02-28 22:52:47 -0800

    Update home.js

--- static/assets/scripts/home.js
+++ static/assets/scripts/home.js
@@ -74,16 +74,7 @@ document.onkeydown = function (evt) {
 }
 
 let splashtext = [
-  'Over 7 Million Users in 2023!',
-  'Fastest growing proxy server!',
-  'Made by Bubbo!',
-  'Check out discord.gg/interstellar :)',
-  'Thanks for using the site',
-  'Follow us on Tiktok (@useinterstellar)',
-  'Subscribe to us on YouTube (@unblocking)',
-  'Subscribe to my Youtube (@xbubbo)',
-  'Check out the settings page',
-  'Check out our Patreon (https://www.patreon.com/gointerstellar)',
+  'Wilson is a fatass',
 ]
 
-document.getElementById('splash').innerText = splashtext[Math.floor(Math.random() * splashtext.length)]
+//document.getElementById('splash').innerText = splashtext[Math.floor(Math.random() * splashtext.length)]
