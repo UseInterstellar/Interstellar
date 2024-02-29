f6969d52b22a4e0f08121bc9b14fe0c1a47e5db4
xbubbo <mrnicebub432@gmail.com>
2024-02-29 17:24:37 -0500

    Fix Ads Not Showing Properly

--- static/assets/scripts/main.js
+++ static/assets/scripts/main.js
@@ -1,31 +1,42 @@
 // Ads
 document.addEventListener('DOMContentLoaded', function () {
+  if (localStorage.getItem('ad') === null) {
+    localStorage.setItem('ad', 'true');
+  }
+
+  if (localStorage.getItem('banner') === null) {
+    localStorage.setItem('banner', 'true');
+  }
+
   if (localStorage.getItem('ad') === 'true') {
-    var advDiv = document.getElementById('adv')
+    var advDiv = document.getElementById('adv');
     if (advDiv) {
-      var script = document.createElement('script')
-      script.type = 'text/javascript'
-      script.src = '//oysterscoldtiny.com/1c/c3/8a/1cc38a6899fdf8ba4dfe779bcc54627b.js'
-      advDiv.appendChild(script)
-      console.log('Script inserted inside the adv div.')
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = '//oysterscoldtiny.com/1c/c3/8a/1cc38a6899fdf8ba4dfe779bcc54627b.js';
+      advDiv.appendChild(script);
+      console.log('Script inserted inside the adv div.');
     }
   }
+
   if (localStorage.getItem('ad') === 'false') {
-    var advDiv = document.getElementById('adv')
+    var advDiv = document.getElementById('adv');
     if (advDiv) {
-      advDiv.remove()
-      console.log('The adv div has been removed.')
+      advDiv.remove();
+      console.log('The adv div has been removed.');
     }
   }
-  var banner = localStorage.getItem('banner') === 'true'
+
+  var banner = localStorage.getItem('banner') === 'true';
   if (!banner) {
-    var bannerDiv = document.getElementById('banner')
+    var bannerDiv = document.getElementById('banner');
     if (bannerDiv) {
-      bannerDiv.remove()
-      console.log('The banner div has been removed.')
+      bannerDiv.remove();
+      console.log('The banner div has been removed.');
     }
   }
-})
+});
+
 // Themes
 var themeid = localStorage.getItem('theme')
 themeEle = document.createElement('link')
