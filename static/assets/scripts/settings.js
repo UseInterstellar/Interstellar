12fda3ddcf43b9e664561d03f0462aece36c2fb6
ypxa <commits@gointerstellar.app>
2024-03-11 09:57:05 -0400

    Custom Search Engine in Settings + Formatted

--- static/assets/scripts/settings.js
+++ static/assets/scripts/settings.js
@@ -288,3 +288,32 @@ function toggleAB() {
     localStorage.setItem('ab', 'true')
   }
 }
+// Search Engine
+function EngineChange(dropdown) {
+  var selectedEngine = dropdown.value
+
+  var engineUrls = {
+    Google: 'https://www.google.com/search?q=',
+    Bing: 'https://www.bing.com/search?q=',
+    DuckDuckGo: 'https://duckduckgo.com/?q=',
+    Qwant: 'https://www.qwant.com/?q=',
+    Startpage: 'https://www.startpage.com/search?q=',
+    SearchEncrypt: 'https://www.searchencrypt.com/search/?q=',
+    Ecosia: 'https://www.ecosia.org/search?q=',
+  }
+
+  localStorage.setItem('engine', engineUrls[selectedEngine])
+  localStorage.setItem('enginename', selectedEngine)
+
+  dropdown.value = selectedEngine
+}
+
+function SaveEngine() {
+  var customEngine = document.getElementById('engine-form').value
+  if (customEngine.trim() !== '') {
+    localStorage.setItem('engine', customEngine)
+    localStorage.setItem('enginename', 'Custom')
+  } else {
+    alert('Please enter a custom search engine value.')
+  }
+}
