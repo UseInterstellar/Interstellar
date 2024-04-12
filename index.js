c6c3f228b8c56fbd7449df98b56b28af1a8f86ad
airtag8892 <120539605+airtag8892@users.noreply.github.com>
2024-04-12 12:40:22 -0400

    privacy policy & tos bc people think interstellar is selling user data !!

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ if (config.routes !== false) {
     { path: '/t', file: 'tabs.html' },
     { path: '/p', file: 'go.html' },
     { path: '/', file: 'index.html' },
+    { path: '/privacy-tos', file: 'tosandprivacy.html' },
   ]
 
   routes.forEach((route) => {
