088d91c46da618e74065315ae0aacaa97f210b56
AntiUSERgaems <150855372+AntiUSERgaems@users.noreply.github.com>
2024-04-23 19:15:40 -0400

    betapassword

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    beta: "password",
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
