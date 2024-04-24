ec2f74d583e882c314d70fa2ad38a7dd6bc9230b
AntiUSERgaems <150855372+AntiUSERgaems@users.noreply.github.com>
2024-04-24 07:59:10 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    beta: "password",
+    hcps: "72FG",
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
