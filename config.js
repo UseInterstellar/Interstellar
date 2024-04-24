7e1743df7bac0fd23afc3304956185a7812c3686
CentauriUX <97356171+F9RDev@users.noreply.github.com>
2024-04-24 09:35:33 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: 'password',
+    interstellar: '',
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
