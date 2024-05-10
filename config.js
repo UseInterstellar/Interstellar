769a1386e59fd6c1fbc02150c6c250909d9579ae
JackGrounds <134026432+JackGrounds@users.noreply.github.com>
2024-05-10 13:42:53 +0000

    update

--- config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  challenge: false, // Set to true if you want to enable password protection.
-  users: {
-    // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
-  },
-}
-export default config
