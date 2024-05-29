5407a2f49b5af8fb5655c4bbf3263912a09a39dd
Bubbo <85169821+xbubbo@users.noreply.github.com>
2024-05-29 16:37:02 +0000

    V5.2.3 - Removed some of the ads + Fixed Idle Breakout + Formatted

--- static/assets/scripts/m.js
+++ static/assets/scripts/m.js
@@ -27,12 +27,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let div2 = document.getElementById("adv2")
   if (div) {
     if (ads === "on") {
-      let scripts = [
-        "//alleygoat.com/4d/2f/92/4d2f92b8c68718dd3efb74b9f9b5fa4e.js",
-        "//alleygoat.com/87/17/b8/8717b825d69c8461460199fb3c6249e4.js",
-        "//alleygoat.com/54/8d/25/548d25a3d0428027eb19da7447bb6c85.js",
-        "//alleygoat.com/1c/c3/8a/1cc38a6899fdf8ba4dfe779bcc54627b.js",
-      ]
+      let scripts = ["//alleygoat.com/4d/2f/92/4d2f92b8c68718dd3efb74b9f9b5fa4e.js", "//alleygoat.com/87/17/b8/8717b825d69c8461460199fb3c6249e4.js"]
 
       scripts.forEach(function (src) {
         let script = document.createElement("script")
@@ -41,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
         div.appendChild(script)
       })
     } else if (ads === "popups") {
-      let scripts = ["//alleygoat.com/4d/2f/92/4d2f92b8c68718dd3efb74b9f9b5fa4e.js", "//alleygoat.com/1c/c3/8a/1cc38a6899fdf8ba4dfe779bcc54627b.js"]
+      let scripts = ["//alleygoat.com/4d/2f/92/4d2f92b8c68718dd3efb74b9f9b5fa4e.js"]
 
       scripts.forEach(function (src) {
         let script = document.createElement("script")
@@ -53,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
       advDiv.remove()
     }
   } else if (div2 && ads != "off2") {
-    let scripts = ["//alleygoat.com/4d/2f/92/4d2f92b8c68718dd3efb74b9f9b5fa4e.js", "//alleygoat.com/1c/c3/8a/1cc38a6899fdf8ba4dfe779bcc54627b.js"]
+    let scripts = ["//alleygoat.com/4d/2f/92/4d2f92b8c68718dd3efb74b9f9b5fa4e.js"]
     scripts.forEach(function (src) {
       let script = document.createElement("script")
       script.type = "text/javascript"
