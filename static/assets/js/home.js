158882450a8a128e9315de105e0621ed3e8e08fc
MinsUR4 <164434993+MinsUR4@users.noreply.github.com>
2024-09-02 13:42:02 -0400

    b

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -14,7 +14,7 @@ if (
   const popup = open("about:blank", "_blank");
   if (!popup || popup.closed) {
     alert(
-      "Please allow popups for this site. Doing so will allow us to open the site in a about:blank tab and preventing this site from showing up in your history. You can turn this off in the site settings.\n\nBy using Interstellar services, you confirm you have read and agreed to the terms listed in our Terms of Service and Privacy Policy, which can be found on the bottom of the settings page.",
+      "Please allow popups for this site. Doing so will allow us to open the site in a about:blank tab and preventing this site from showing up in your history. You can turn this off in the site settings.\n\nBy using Hello services, you confirm you have read and agreed to the terms listed in our Terms of Service and Privacy Policy, which can be found on the bottom of the settings page.",
     );
   } else {
     const doc = popup.document;
@@ -175,13 +175,13 @@ const SplashT = [
   "Over 8 Million Users since 2023",
   "Fastest growing proxy server",
   "Made by xBubbo",
-  "Check out discord.gg/interstellar :)",
+  "Check out discord.gg/Hello :)",
   "Thanks for using the site",
-  "Follow us on Tiktok (@useinterstellar)",
+  "Follow us on Tiktok (@useHello)",
   "Subscribe to us on YouTube (@unblocking)",
   "Subscribe to my Youtube (@xbubbo)",
   "Check out the settings page",
-  "Check out our Patreon (https://www.patreon.com/gointerstellar)",
+  "Check out our Patreon (https://www.patreon.com/goHello)",
 ];
 
 let SplashI = Math.floor(Math.random() * SplashT.length);
