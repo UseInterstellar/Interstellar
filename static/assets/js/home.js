d35dd81fb12e6908f952b73b256e0dc316ae2f53
Tmoneytroyt <163927622+Tmoneytroyt@users.noreply.github.com>
2024-09-26 15:12:55 -0500

    Update home.js

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -172,16 +172,7 @@ document.addEventListener("DOMContentLoaded", event => {
 });
 // Splash texts
 const SplashT = [
-  "Over 8 Million Users since 2023",
-  "Fastest growing proxy server",
-  "Made by xBubbo",
-  "Check out discord.gg/interstellar :)",
-  "Thanks for using the site",
-  "Follow us on Tiktok (@useinterstellar)",
-  "Subscribe to us on YouTube (@unblocking)",
-  "Subscribe to my Youtube (@xbubbo)",
-  "Check out the settings page",
-  "Check out our Patreon (https://www.patreon.com/gointerstellar)",
+  "Dont leak my hardwork",
 ];
 
 let SplashI = Math.floor(Math.random() * SplashT.length);
