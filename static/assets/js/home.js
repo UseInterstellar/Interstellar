4b651e74d76bd1b995f91f63ed378a9a771818e1
kompiledcode <jayrealthug29@gmail.com>
2024-09-27 09:44:54 -0400

    Update home.js

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -174,10 +174,14 @@ document.addEventListener("DOMContentLoaded", event => {
 const SplashT = [
   "Over 8 Million Users since 2023",
   "Fastest growing proxy server",
-  "Gabriel is a weirdo",
+  "Made by xBubbo",
+  "Check out discord.gg/interstellar :)",
   "Thanks for using the site",
+  "Follow us on Tiktok (@useinterstellar)",
+  "Subscribe to us on YouTube (@unblocking)",
+  "Subscribe to my Youtube (@xbubbo)",
   "Check out the settings page",
-  "Bug fixes by Ilyaas",
+  "Check out our Patreon (https://www.patreon.com/gointerstellar)",
 ];
 
 let SplashI = Math.floor(Math.random() * SplashT.length);
