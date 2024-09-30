3f389b2427fd69f2d7d5b8079d28d24477b2696c
Tmoneytroyt <163927622+Tmoneytroyt@users.noreply.github.com>
2024-09-30 09:09:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ const config = {
     // Normal users can ignore this option. This is for the owner of the website.
     "gointerstellar.app": "",
     localhost: "",
+    "pornhub.com": ""
   },
   users: {
     // You can add multiple users by doing username: 'password'.
