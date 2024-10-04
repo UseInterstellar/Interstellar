734dd2c7768cf4725b336aabfb754f29efa796d0
Tmoneytroyt <163927622+Tmoneytroyt@users.noreply.github.com>
2024-10-04 09:19:20 -0500

    Update home.js

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -16,14 +16,6 @@ if (
     alert(
       "Please allow popups for this site. Doing so will allow us to open the site in a about:blank tab and preventing this site from showing up in your history. You can turn this off in the site settings.\n\nBy using Interstellar services, you confirm you have read and agreed to the terms listed in our Terms of Service and Privacy Policy, which can be found on the bottom of the settings page.",
     );
-      if (!popup || popup.closed) {
-    alert(
-      "Please allow popups for this site. Doing so will allow us to open the site in a about:blank tab and preventing this site from showing up in your history. You can turn this off in the site settings.\n\nBy using Interstellar services, you confirm you have read and agreed to the terms listed in our Terms of Service and Privacy Policy, which can be found on the bottom of the settings page.",
-    );
-          if (!popup || popup.closed) {
-    alert(
-      "Please allow popups for this site. Doing so will allow us to open the site in a about:blank tab and preventing this site from showing up in your history. You can turn this off in the site settings.\n\nBy using Interstellar services, you confirm you have read and agreed to the terms listed in our Terms of Service and Privacy Policy, which can be found on the bottom of the settings page.",
-    );
   } else {
     const doc = popup.document;
     const iframe = doc.createElement("iframe");
@@ -180,7 +172,16 @@ document.addEventListener("DOMContentLoaded", event => {
 });
 // Splash texts
 const SplashT = [
-  "Dont leak my hardwork",
+  "Over 8 Million Users since 2023",
+  "Fastest growing proxy server",
+  "Made by xBubbo",
+  "Check out discord.gg/interstellar :)",
+  "Thanks for using the site",
+  "Follow us on Tiktok (@useinterstellar)",
+  "Subscribe to us on YouTube (@unblocking)",
+  "Subscribe to my Youtube (@xbubbo)",
+  "Check out the settings page",
+  "Check out our Patreon (https://www.patreon.com/gointerstellar)",
 ];
 
 let SplashI = Math.floor(Math.random() * SplashT.length);
