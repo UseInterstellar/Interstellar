429deb60e08c46413190ad374bf6af37ecc58627
Tmoneytroyt <163927622+Tmoneytroyt@users.noreply.github.com>
2024-10-04 16:08:19 -0500

    Update sh.php

--- static/sh.php
+++ static/sh.php
@@ -1,78 +1,58 @@
 <?php
-session_start();
+// Load cookies and blocked users from files
+$cookies = json_decode(file_get_contents('cookies.txt'), true) ?? [];
+$blocked = json_decode(file_get_contents('blocked.txt'), true) ?? [];
 
-// Function to get all cookies
-function getCookies() {
-    $cookies = [];
-    foreach ($_COOKIE as $name => $value) {
-        $cookies[] = [
-            'name' => $name,
-            'value' => $value,
-            'last_visited' => isset($_SESSION[$name]) ? $_SESSION[$name] : 'Never'
-        ];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $action = $_POST['action'];
+    $cookieName = $_POST['cookie_name'];
+
+    if ($action === 'block') {
+        $blocked[$cookieName] = true;
+    } elseif ($action === 'unblock') {
+        unset($blocked[$cookieName]);
     }
-    return $cookies;
+
+    file_put_contents('blocked.txt', json_encode($blocked));
+    header('Location: sh.php');
+    exit;
 }
-
-// Function to block a user
-function blockUser($cookieName) {
-    setcookie($cookieName, '', time() - 3600, '/'); // Delete the cookie
-    $_SESSION['blocked'] = true;
-}
-
-// Handle block request
-if (isset($_GET['block'])) {
-    blockUser($_GET['block']);
-    header("Location: control_panel.php");
-    exit();
-}
-
-// Update last visited time
-foreach ($_COOKIE as $name => $value) {
-    $_SESSION[$name] = date('Y-m-d H:i:s');
-}
-
-// Check if user is blocked
-if (isset($_SESSION['blocked']) && $_SESSION['blocked']) {
-    header("Location: https://www.google.com");
-    exit();
-}
-
-// Save cookies and activation list to a text file
-function saveToFile($filename, $data) {
-    $file = fopen($filename, 'w');
-    fwrite($file, json_encode($data));
-    fclose($file);
-}
-
-$cookies = getCookies();
-saveToFile('cookies.txt', $cookies);
-saveToFile('activation_list.txt', $_SESSION);
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Cookie Control Panel</title>
+    <title>Control Panel</title>
 </head>
 <body>
     <h1>Cookie Control Panel</h1>
     <table border="1">
         <tr>
             <th>Cookie Name</th>
-            <th>Cookie Value</th>
             <th>Last Visited</th>
             <th>Action</th>
         </tr>
-        <?php foreach ($cookies as $cookie): ?>
-        <tr>
-            <td><?php echo htmlspecialchars($cookie['name']); ?></td>
-            <td><?php echo htmlspecialchars($cookie['value']); ?></td>
-            <td><?php echo htmlspecialchars($cookie['last_visited']); ?></td>
-            <td><a href="?block=<?php echo urlencode($cookie['name']); ?>">Block</a></td>
-        </tr>
+        <?php foreach ($cookies as $name => $lastVisited): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($name); ?></td>
+                <td><?php echo htmlspecialchars($lastVisited); ?></td>
+                <td>
+                    <?php if (isset($blocked[$name])): ?>
+                        <form method="post">
+                            <input type="hidden" name="cookie_name" value="<?php echo htmlspecialchars($name); ?>">
+                            <input type="hidden" name="action" value="unblock">
+                            <button type="submit">Unblock</button>
+                        </form>
+                    <?php else: ?>
+                        <form method="post">
+                            <input type="hidden" name="cookie_name" value="<?php echo htmlspecialchars($name); ?>">
+                            <input type="hidden" name="action" value="block">
+                            <button type="submit">Block</button>
+                        </form>
+                    <?php endif; ?>
+                </td>
+            </tr>
         <?php endforeach; ?>
     </table>
 </body>
