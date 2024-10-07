30bb6bdcafb8795de4144c4ee97470e7d0316b87
Bubbo <85169821+xbubbo@users.noreply.github.com>
2024-10-07 05:43:14 +0000

    V5.2.6 - Added ads back

--- static/assets/js/m.js
+++ static/assets/js/m.js
@@ -1,11 +1,11 @@
-/* document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
   if (!document.getElementById("no")) {
     const script = document.createElement("script");
     script.type = "text/javascript";
-    script.src = "//alleygoat.com/5e/6b/27/5e6b2776400180cc548a7dfd8ab3f717.js";
+    script.src = "//pl24619107.cpmrevenuegate.com/e1/6b/a5/e16ba5dfa875317d6e76da1e591f9e90.js";
     document.body.appendChild(script);
   }
-}); */
+});
 
 // Dynamic
 document.addEventListener("DOMContentLoaded", () => {
