48264c93e6f63439a5116e583f1df18271b8382d
HumanV21 <hummama25@gmail.com>
2024-10-28 13:46:22 -0400

    Update config.js
    pl
    s work

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   blocked: {
     // Normal users can ignore this option. This is for the owner of the website.
     "gointerstellar.app": "",
