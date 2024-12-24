7fec258fc6a5abad7a4747133f25932c1b8b17d5
jakahassbshk <exotic.marioofficial@gmail.com>
2024-12-24 10:00:34 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
     interstellar: "password",
