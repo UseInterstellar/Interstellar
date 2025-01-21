8407c6a958b0f0df8ac67cb57f93d511a0d99ca0
thonk <hello@villainsrule.xyz>
2025-01-21 00:16:29 -0500

    improve youtube support

--- static/assets/js/t.js
+++ static/assets/js/t.js
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", event => {
     const tabTitle = document.createElement("span");
     const newIframe = document.createElement("iframe");
     newIframe.sandbox =
-      "allow-same-origin allow-scripts allow-forms allow-pointer-lock allow-modals allow-orientation-lock";
+      "allow-same-origin allow-scripts allow-forms allow-pointer-lock allow-modals allow-orientation-lock allow-presentation allow-storage-access-by-user-activation";
     // When Top Navigation is not allowed links with the "top" value will be entirely blocked, if we allow Top Navigation it will overwrite the tab, which is obviously not wanted.
     tabTitle.textContent = `New Tab ${tabCounter}`;
     tabTitle.className = "t";
