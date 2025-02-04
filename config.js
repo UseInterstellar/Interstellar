280de43cde62ed01728764d0e4147697920329f4
chainsfr770 <elrodmorgan1989@gmail.com>
2025-02-04 08:33:26 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  true: false, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
     interstellar: "password",
