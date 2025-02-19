e3063313acce4e88c2fdc6ec501d0a3314a25a61
XYZ-thech <ujmy318@gmail.com>
2025-02-19 10:48:00 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "Flecisbad",
+    Adriel: "Flecisbad",Gio: "Flecisbad",
   },
 };
 
