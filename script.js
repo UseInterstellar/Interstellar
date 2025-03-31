6f2ade4e5aec73d9a9ff7f2229f0ec990d24cb61
coolestcoder655 <74312880+coolestcoder655@users.noreply.github.com>
2025-03-31 16:50:18 -0500

    Update and rename Script.js to script.js

--- script.js
+++ script.js
@@ -11,30 +11,35 @@ app.use(session({
   cookie: { secure: false }
 }));
 
-// Track active users
 let activeUsers = {};
 
 app.post('/login', (req, res) => {
+  console.log('Login attempt:', req.body);
   const username = req.body.username;
 
   if (activeUsers[username]) {
+    console.log('User already logged in:', username);
     return res.status(403).send('User is already logged in');
   }
 
   req.session.username = username;
   activeUsers[username] = true;
+  console.log('User logged in:', username);
   res.send('Login successful');
 });
 
 app.post('/logout', (req, res) => {
   const username = req.session.username;
+  console.log('Logout attempt:', username);
 
   if (username) {
     delete activeUsers[username];
     req.session.destroy(err => {
       if (err) {
+        console.log('Logout failed:', err);
         return res.status(500).send('Logout failed');
       }
+      console.log('User logged out:', username);
       res.send('Logout successful');
     });
   } else {
