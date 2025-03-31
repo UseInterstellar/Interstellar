a4504966babad4a779f801d6b53731e11777ed24
coolestcoder655 <74312880+coolestcoder655@users.noreply.github.com>
2025-03-31 20:11:59 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
     admin: "sigmaMale",
+    littleTim2359: "Litten123"
   },
 };
 
