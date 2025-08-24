361bfb9be226a26c56f94238ad29334b342ca918
kingofkings011 <isaiahwhi4851@k12.ipsd.org>
2025-08-24 10:33:32 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  challenge: true, // Set to true if you want to enable password protection.
+  challenge: false, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
     interstellar: "password",
