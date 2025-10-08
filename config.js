8c8f03030543bed5f2de51366e5edecedb966427
cleverman606 <76454906+cleverman606@users.noreply.github.com>
2025-10-08 16:30:28 +0000

    Pending changes exported from your codespace

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    mysterio: "Yiannis123!",
+    jayden: "Nedyaj",
+    goblue: "gobluetmk",
   },
 };
 
