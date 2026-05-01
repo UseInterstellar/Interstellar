a1b7926e8498a6ce416d2c3ac75235d90ddbaf65
xbubbo <commits@gointerstellar.app>
2026-05-01 12:17:52 -0400

    fix(particles): fix loading, interactivity, and update config

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -50,41 +50,36 @@ if (!inFrame && !navigator.userAgent.includes("Firefox") && localStorage.getItem
 }
 // Particles
 document.addEventListener("DOMContentLoaded", event => {
-  if (window.localStorage.getItem("Particles") === "true") {
+  if (window.localStorage.getItem("particles") === "true") {
     const particlesConfig = {
       particles: {
         number: {
-          value: 200,
+          value: 350,
           density: {
             enable: true,
-            value_area: 600,
+            value_area: 800,
           },
         },
         color: {
           value: "#ffffff",
         },
         shape: {
-          type: "circle",
+          type: "star",
           stroke: {
             width: 0,
-            color: "#000000",
+            color: "#ffffff",
           },
           polygon: {
             nb_sides: 5,
-          },
-          image: {
-            src: "img/github.svg",
-            width: 100,
-            height: 100,
           },
         },
         opacity: {
           value: 1,
           random: true,
           anim: {
-            enable: false,
-            speed: 1,
-            opacity_min: 0.1,
+            enable: true,
+            speed: 0.9,
+            opacity_min: 0,
             sync: false,
           },
         },
@@ -92,70 +87,40 @@ document.addEventListener("DOMContentLoaded", event => {
           value: 3,
           random: true,
           anim: {
-            enable: false,
-            speed: 40,
-            size_min: 0.1,
+            enable: true,
+            speed: 1.1,
+            size_min: 0.3,
             sync: false,
           },
         },
         line_linked: {
           enable: false,
-          distance: 150,
-          color: "#ffffff",
-          opacity: 0.4,
-          width: 1,
         },
         move: {
           enable: true,
-          speed: 2,
-          direction: "bottom",
+          speed: 3,
+          direction: "none",
           random: true,
-          straight: false,
+          straight: true,
           out_mode: "out",
           bounce: false,
-          attract: {
-            enable: false,
-            rotateX: 600,
-            rotateY: 1200,
-          },
         },
       },
       interactivity: {
         detect_on: "canvas",
         events: {
           onhover: {
-            enable: true,
-            mode: "repulse",
+            enable: false,
           },
           onclick: {
-            enable: false,
+            enable: true,
             mode: "push",
           },
           resize: true,
         },
         modes: {
-          grab: {
-            distance: 400,
-            line_linked: {
-              opacity: 1,
-            },
-          },
-          bubble: {
-            distance: 400,
-            size: 40,
-            duration: 2,
-            opacity: 8,
-            speed: 3,
-          },
-          repulse: {
-            distance: 40,
-            duration: 0.4,
-          },
           push: {
-            particles_nb: 4,
-          },
-          remove: {
-            particles_nb: 2,
+            particles_nb: 30,
           },
         },
       },
